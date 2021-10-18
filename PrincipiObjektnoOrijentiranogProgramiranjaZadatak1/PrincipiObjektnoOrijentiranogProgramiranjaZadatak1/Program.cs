d69c17b2a58f92efcce86263d0a40d6023ed87dc
IvanAbuzimbale <58472137+IvanAbuzimbale@users.noreply.github.com>
2021-10-18 09:11:54 +0200

    Add files via upload

---
+++ PrincipiObjektnoOrijentiranogProgramiranjaZadatak1/PrincipiObjektnoOrijentiranogProgramiranjaZadatak1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiObjektnoOrijentiranogProgramiranjaZadatak1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sport nogomet = new Sport("Nogomet", true, 11);
+            Sport vaterpolo = new Sport("Vaterpolo", true, 7);
+            Sport curling = new Sport("Curling", false, 5);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PrincipiObjektnoOrijentiranogProgramiranjaZadatak1/PrincipiObjektnoOrijentiranogProgramiranjaZadatak1/Sport.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrincipiObjektnoOrijentiranogProgramiranjaZadatak1
+{
+    class Sport
+    {
+        private string Naziv;
+        private bool IgraLiSeLoptom;
+        private byte BrojIgraca;
+
+        public Sport(string naziv)
+        {
+            Naziv = naziv;
+        }
+        public Sport(string naziv, bool igralise)
+        {
+            Naziv = naziv;
+            IgraLiSeLoptom = igralise;
+        }
+        public Sport(string naziv, bool igralise, byte broj)
+        {
+            Naziv = naziv;
+            IgraLiSeLoptom = igralise;
+            BrojIgraca = broj;
+        }
+    }
+}
